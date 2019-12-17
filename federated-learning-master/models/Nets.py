92090bc1d3f4d7ade1570c169997f1e53e0ed356
Vivek Khimani <vivekkhimani07@gmail.com>
2019-12-17 10:46:19 +0530

    updated the customized cifar cnn model

--- federated-learning-master/models/Nets.py
+++ federated-learning-master/models/Nets.py
@@ -63,42 +63,60 @@ class CNNCifar(nn.Module):
         x = self.fc3(x)
         return F.log_softmax(x, dim=1)
 
+class Unit(nn.Module):
+    def __init__(self,in_channels,out_channels):
+        super(Unit,self).__init__()
+
+        self.conv = nn.Conv2d(in_channels=in_channels,kernel_size=3,out_channels=out_channels,stride=1,padding=1)
+        self.bn = nn.BatchNorm2d(num_features=out_channels)
+        self.relu = nn.ReLU()
+
+    def forward(self,input):
+        output = self.conv(input)
+        output = self.bn(output)
+        output = self.relu(output)
+
+        return output
+
 
 class customCNNCifar(nn.Module):
     def __init__(self, args):
         super(customCNNCifar, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=12, kernel_size=3, stride=1,padding=1)
-        self.relu1 = nn.ReLU()
 
-        self.conv2 = nn.Conv2d(in_channels=12, out_channels=12, kernel_size=3, stride=1, padding=1)
-        self.relu2 = nn.ReLU()
+        #14 layers of unit with max pooling in between
+        self.unit1 = Unit(in_channels=3,out_channels=32)
+        self.unit2 = Unit(in_channels=32,out_channels=32)
+        self.unit3 = Unit(in_channels=32,out_channels=32)
 
-        self.pool = nn.MaxPool2d(kernel_size=2)
+        self.pool1 = nn.MaxPool2d(kernel_size=2)
 
-        self.conv3 = nn.Conv2d (in_channels=12, out_channels=24, kernel_size=3, stride=1, padding=1)
-        self.relu3 = nn.ReLU()
+        self.unit4 = Unit(in_channels=32,out_channels=64)
+        self.unit5 = Unit(in_channels=64,out_channels=64)
+        self.unit6 = Unit(in_channels=64,out_channels=64)
+        self.unit7 = Unit(in_channels=64,out_channels=64)
 
-        self.conv4 = nn.Conv2d (in_channels=24, out_channels=24, kernel_size=3, stride=1, padding=1)
-        self.relu4 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(kernel_size=2)
 
-        self.fc = nn.Linear(in_features=16*16*24, out_features=args.num_classes)
+        self.unit8 = Unit(in_channels=64,out_channels=128)
+        self.unit9 = Unit(in_channels=128,out_channels=128)
+        self.unit10 = Unit(in_channels=128,out_channels=128)
+        self.unit11 = Unit(in_channels=128,out_channels=128)
+
+        self.pool3 = nn.MaxPool2d(kernel_size=2)
+
+        self.unit12 = Unit(in_channels=128,out_channels=128)
+        self.unit13 = Unit(in_channels=128,out_channels=128)
+        self.unit14 = Unit(in_channels=128,out_channels=128)
+
+        self.avgpool = nn.AvgPool2d(kernel_size=4)
+
+        #add all the units in-order in a sequential layer
+        self.net = nn.Sequential(self.unit1,self.unit2,self.unit3,self.pool1,self.unit4,self.unit5,self.unit6,self.unit7,self.pool2,self.unit8,self.unit9,self.unit10,self.unit11,self.pool3,self.unit12,self.unit13,self.unit14,self.avgpool)
+
+        self.fc = nn.Linear(in_features=128,out_features=args.num_classes)
 
     def forward(self,input):
-        output = self.conv1(input)
-        output = self.relu1(output)
-
-        output = self.conv2(output)
-        output = self.relu2(output)
-
-        output = self.pool(output)
-
-        output = self.conv3(output)
-        output = self.relu3(output)
-
-        output = self.conv4(output)
-        output = self.relu4(output)
-
-        output = output.view(-1,16*16*24)
+        output = self.net(input)
+        output = output.view(-1,128)
         output = self.fc(output)
-
         return output
