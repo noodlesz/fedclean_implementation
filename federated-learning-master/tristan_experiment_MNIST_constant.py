fa90f22e54d62a3724521017b6679ff3067cdd0c
Vivek Khimani <vivekkhimani07@gmail.com>
2020-01-19 09:29:37 -0500

    reverted the changes

--- federated-learning-master/tristan_experiment_MNIST_constant.py
+++ federated-learning-master/tristan_experiment_MNIST_constant.py
@@ -260,8 +260,8 @@ if __name__ == '__main__':
         loss_avg_10 = sum(loss_locals_10) / len(loss_locals_10)
 
         non_malicious_structure[iter][0] = loss_avg
-        non_malicious_structure5[iter][0] = loss_avg_5
-        non_malicious_structure10[iter][0] = loss_avg_10
+        non_malicious_structure5[iter][0] = loss_avg5
+        non_malicious_structure10[iter][0] = loss_avg10
 
         malicious_structure5[iter][0] = loss_avg5
         malicious_structure10[iter][0] = loss_avg10
